5a6c4ca1d79ce793e8726cddf72077d8ca28b122
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-18 02:56:17 +0530

    Routes to home on Logo Click

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { INDEX } from '../../helpers/RoutesURL';
 
 const Footer = () => {
     return (
         <footer className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-                    <img src="./images/nlogo.png" width="50px" />
-                    <span className="ml-3 text-xl">Novies</span>
-                </a>
+                <Link to={INDEX}>
+                    <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                        <img src="./images/nlogo.png" width="50px" />
+                        <span className="ml-3 text-xl">Novies</span>
+                    </a>
+                </Link>
                 <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2021 Novies —
                     <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@numDev</a>
                 </p>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { FAVORITE_MOVIE } from '../../helpers/RoutesURL';
+import { FAVORITE_MOVIE, INDEX } from '../../helpers/RoutesURL';
 
 const Header = () => {
     const { favoriteMovies } = useSelector(state => state.moviesReducer);
@@ -9,11 +9,12 @@ const Header = () => {
     return (
         <header className="text-gray-400 bg-gray-900 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-                    <img src="./images/nlogo.png" width="50px" />
-                    <span className="ml-3 text-xl">Novies</span>
-                </a>
-
+                <Link to={INDEX}>
+                    <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                        <img src="./images/nlogo.png" width="50px" />
+                        <span className="ml-3 text-xl">Novies</span>
+                    </a>
+                </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     {/* <a className="mr-5 hover:text-white">First Link</a> */}
                 </nav>
