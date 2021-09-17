eec1b029bac4d3c5d55b55a654aebeb2a1bf42b7
Numan Ahmed <mdnmnahmed@gmail.com>
2021-09-17 20:31:35 +0530

    Created Helper Function to get Data from the API, Base URL & .env file added

---
+++ src/helpers/APIRequestHelper.js
@@ -0,0 +1,58 @@
+/*
+ * Filename: APIRequestHelper.js
+ * Created Date: Saturday, September 16th 2021, 11:00:21 pm
+ * Author: Md Numan Ahmed
+ * 
+ * Copyright (c) 2021 Numan-Dev
+*/
+
+import axios from "axios";
+
+
+/**
+ * Fetch Data from the API & return Response
+ * @param {String} method - HTTP requests - GET || POST || PUT || DELETE
+ * @param {String} endpoint 
+ * @param {Object} data 
+ * @returns - {Object} - API response
+ */
+const APIRequestHelper = async (method = 'get', endpoint, data = null) => {
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+    let config = {};
+
+    if ((method === 'post' || method === 'put') && data) {
+        config = {
+            method: method,
+            url: BASE_URL + endpoint,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': accessToken
+            },
+            data: data
+        };
+    }
+
+    if (method === 'get') {
+        config = {
+            method: 'get',
+            url: BASE_URL + endpoint,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': accessToken
+            },
+        };
+    }
+
+    let response = await axios(config)
+        .catch((error) => {
+            if (error.response) {
+                if (error.response.data.msg) {
+                    console.error('Error occur in Fetching data from API: ', error.response.data.msg);
+                }
+            }
+        });
+
+    return response;
+}
+
+export default APIRequestHelper;
